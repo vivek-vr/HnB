c286a864335e1629eefe4dd239af49540d7ce75b
Vivek Velappannair Radha <Vivek_Velappannair@ad.infosys.com>
2018-09-02 18:17:41 +0530

    integrating Angular

--- public/app/controllers/productListController.js
+++ public/app/controllers/productListController.js
@@ -9,10 +9,67 @@ hbiApp.controller('productListController', ['$scope','$http', 'productlistServic
 			$scope.products = response.data.results;
 			console.log("$scope.products");
 			console.log($scope.products);
+			$scope.setPageCount(response);
 		});
 	}
+	
+	$scope.onProductSort = function(sortOption){
+		//console.log('Sorting Item selected : ',sortOption);
+		var sortType = "";
+		switch (sortOption) {
+            case 'sort_low-high' :
+				sortType = "price%20asc";
+                break;
+            case 'sort_high-low':
+				sortType = "price%20desc";
+				break;
+			case 'sort_a-to-z' :
+				sortType = "name.en%20asc";
+                break;
+            case 'sort_z-to-a':
+				sortType = "name.en%20desc";
+				break;
+			case 'sort_ratings':
+				sortType = "reviewRatingStatistics.highestRating%20asc";
+				break;
+			case 'popularity':
+				sortType = "popularity";
+				break;
+				
+            default:
+
+		}
+		if(sortType == "popularity"){
+			productlistService.getProducts($stateParams.categoryId)  // get all products based on category
+			.then(function(response) {
+				$scope.products = response.data.results;
+				$scope.setPageCount(response);
+			});	
+		} else {
+			productlistService.getSortedProducts($stateParams.categoryId, sortType)  // get all sorted products based on category
+			.then(function(response) {
+				$scope.products = response.data.results;
+				$scope.setPageCount(response);
+			});
+		}
+		
+	}
+	
 	$scope.setProduct = function(id) {
 		headerService.sessionSet("productId",id);
 	}
-    
+	
+	$scope.setPageCount = function(productList){
+		$scope.totalProducts = productList.data.total;
+		$scope.countOfProducts = productList.data.count;
+		$scope.items = parseInt($scope.countOfProducts/20);
+		$scope.steps = [];
+		if($scope.items == 0){
+			$scope.steps.push("All");
+		} else{
+			for(var i=1;i<=$scope.items;i++) {
+				$scope.steps.push(i*20);
+			}
+		}
+	}
 }]);
