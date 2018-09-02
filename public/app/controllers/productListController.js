4de3b4938d412bf590108bbe7bf257fabf415cd0
Vivek Velappannair Radha <Vivek_Velappannair@ad.infosys.com>
2018-09-02 15:17:16 +0530

    integrating Angular

--- public/app/controllers/productListController.js
+++ public/app/controllers/productListController.js
@@ -2,6 +2,7 @@ hbiApp.controller('productListController', ['$scope','$http', 'productlistServic
 	
 	$scope.init = function(){
 		var categoryId = headerService.sessionGet('categoryId');
+		if(categoryId == null) { categoryId = '1a925ebe-77fe-4bf3-abc9-676fe1b64df1';}
 		// productlistService.getProducts($stateParams.categoryId)  // get all products based on category
 		productlistService.getProducts(categoryId)
 		.then(function(response) {
