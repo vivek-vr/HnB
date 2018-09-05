d3ab2484df8f6025330f32379a8bf9c19501b609
vivek <vivek_velappannair@infosys.com>
2018-09-05 12:21:14 +0530

    responsive changes

---
+++ public/app/services/cartService - Copy.js
@@ -0,0 +1,60 @@
+hbiApp.factory('cartService', function($http, headerService,productlistService, $q) {
+	return {
+		cartActions: function (action, productId,varient,quantity) {
+			this.initCart();
+			var cart = headerService.sessionGet('cart');
+			var input = {};
+			input.version = cart.version;
+			var actions = [];
+			var lineActions = {};
+			lineActions.action = action;
+			lineActions.productId = productId;
+			lineActions.variantId = varient;
+			lineActions.quantity = quantity;
+			actions.push(lineActions);
+			input.actions = actions;
+			productlistService.cartActions(cart,input).then(function(response) {
+				cart.id = response.data.id;
+				cart.version = response.data.version;
+				headerService.sessionSet('cart', cart);
+				var cust = {};
+				cust.id = response.data.customerId;
+				headerService.sessionSet('customer', cust);
+				$('.js-header-basket-link').find('.price').html(' £'+(response.data.totalPrice.centAmount)/100+' ');
+				$('.at-basket-menu-qty').html(' '+response.data.lineItems.length+' ');
+
+			});
+		},
+		initCart: function () {
+			var cart = headerService.sessionGet('cart');
+			if (!cart || Object.keys(cart).length == 0) {
+				this.createCart();
+			}
+		},
+		createCart: function() {
+			var data = {};
+			var cart = {};
+			data.currency = 'GBP'; // get from header
+			var custId = this.getCustId(); 
+			if(custId != null){
+			  data.customerId =custId			
+			}
+			productlistService.createCart(data).then(function(response) {
+				if(response.data){
+					cart.id = response.data.id;
+					cart.version = response.data.version;
+					headerService.sessionSet('cart', cart);
+					var cust = {};
+					cust.id = response.data.customerId;
+					headerService.sessionSet('customer', cust);
+				}
+				
+			});
+		},
+		getCustId: function() {
+			var customer = headerService.sessionGet('customer');
+			return customer == null?null:customer.id;
+		}
+		
+}
+});
